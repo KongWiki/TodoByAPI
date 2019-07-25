b024413ffecef5f5301809c0b4ef4a4723037992
KongWiki <kongwiki@163.com>
2019-07-25 14:07:54 +0800

    complete base config

--- app/config.py
+++ app/config.py
@@ -4,3 +4,36 @@
 @File: config.py
 @Email: kongwiki@163.com
 """
+import os
+
+basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+
+class BaseConfig:
+    TODOISM_ITEM_PER_PAGE = 20
+
+    SECRET_KEY = os.getenv('SECRET_KEY', 'a secret string')
+
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///' + os.path.join(basedir, 'data.db'))
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(BaseConfig):
+    pass
+
+
+class ProductionConfig(BaseConfig):
+    pass
+
+
+class TestingConfig(BaseConfig):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///'
+    WTF_CSRF_ENABLED = False
+
+
+config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'testing': TestingConfig
+}
